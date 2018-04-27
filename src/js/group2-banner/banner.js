1b67234c050773206a5817364323f73653dd636e
Dominic Brant <dombrant@gmail.com>
2018-04-26 17:37:41 -0700

    added window.onload function to change the background of .banner

--- src/js/group2-banner/banner.js
+++ src/js/group2-banner/banner.js
@@ -1,3 +1,9 @@
+window.onload = () => {
+  document.querySelector('.banner').classList.add('banner-image-background');
+};
+// Change the background of .banner from a blue color to the image with palm trees
+// The class banner-image-background is in banner-style.css
+
 const scrollToAbout = () => {
   smoothScroll(document.querySelector('.main-about'), 500, 'easeInQuad');
 };
