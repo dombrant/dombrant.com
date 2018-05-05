b1892c520272d1b854e6168d328a661feea3969a
Dominic Brant <dombrant@gmail.com>
2018-05-04 20:58:04 -0700

    refactored mainDivsFadeIn to an object

--- src/js/group3-main/main-sections.js
+++ src/js/group3-main/main-sections.js
@@ -1,9 +1,23 @@
-const mainDivsFadeIn = () => {
-  document.querySelectorAll('.main > div').forEach(div => {
-    if (isInView(div)) {
-      div
-        .querySelectorAll('*')
-        .forEach(element => element.classList.add('fade-in-on-scroll'));
-    }
-  });
+const mainDivsFadeIn = {
+  completed: false,
+
+  run() {
+    const sections = document.querySelectorAll('.main > div');
+
+    sections.forEach((div, index) => {
+      if (isInView(div)) {
+        div
+          .querySelectorAll('*')
+          .forEach(element => element.classList.add('fade-in-on-scroll'));
+
+        if (index === sections.length - 1) {
+          // If the animation is being applied to the last element in the sections array
+
+          this.completed = true;
+          // Set completed to true
+          // This will be used in scroll-listener.js to stop the requestAnimationFrame for this function
+        }
+      }
+    });
+  }
 };
