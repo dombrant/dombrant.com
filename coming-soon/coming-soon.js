7836e39edf6bb9f832254bb81fde15fb324f6430
Dominic Brant <dombrant@gmail.com>
2018-05-17 20:48:57 -0700

    created coming-soon.js to handle image loading

---
+++ coming-soon/coming-soon.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+  const backgroundImage = new Image();
+  backgroundImage.src =
+    'https://media.githubusercontent.com/media/dombrant/dombrant.com/master/img/palm-trees-6EBBDE.jpg';
+  backgroundImage.onload = () => {
+    document
+      .querySelector('.container')
+      .classList.add('banner-image-background');
+  };
+};
+// Change the background of .container from a blue color to the image with palm trees
+// The class banner-image-background is in coming-soon.css
