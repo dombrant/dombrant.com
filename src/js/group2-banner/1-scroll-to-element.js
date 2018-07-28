623252a324e14994b12673f7420872f28b1c2a38
Dominic Brant <dombrant@gmail.com>
2018-07-27 19:36:10 -0700

    got scrolling working for all sections by using offsetTop

--- src/js/group2-banner/1-scroll-to-element.js
+++ src/js/group2-banner/1-scroll-to-element.js
@@ -1,16 +1,7 @@
 const scrollToElement = element => {
-  const elementHeight = () => {
-    document.querySelector(element).getBoundingClientRect().top +
-      window.pageYOffset -
-      document.documentElement.clientTop;
-
-    console.log(elementHeight);
-  };
-
   const headerHeight = document.querySelector('header').getBoundingClientRect()
     .height;
-  const elementTop = document.querySelector(element).getBoundingClientRect()
-    .top;
+  const elementTop = document.querySelector(element).offsetTop;
   // Get the height of the header and the top position of the element, these will be used to calculate where to scroll to when scrolling to the about section below
   // Instead of scrolling to the top of the section, it will scroll to the top of the section minus the height of the header, so that the header does not cover up the top of the section
 
