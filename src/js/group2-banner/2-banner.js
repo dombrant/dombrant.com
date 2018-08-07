747c11d006136c2b4622d7a2a2be35372dcfbfb8
Dominic Brant <dombrant@gmail.com>
2018-08-07 14:54:49 -0700

    moved window.onload code to onload.js

--- src/js/group2-banner/2-banner.js
+++ src/js/group2-banner/2-banner.js
@@ -1,14 +1,3 @@
-window.onload = () => {
-  const backgroundImage = new Image();
-  backgroundImage.src =
-    'https://media.githubusercontent.com/media/dombrant/dombrant.com/master/img/palm-trees-6EBBDE.jpg';
-  backgroundImage.onload = () => {
-    document.querySelector('.banner').classList.add('banner-image-background');
-  };
-};
-// Change the background of .banner from a blue color to the image with palm trees
-// The class banner-image-background is in banner-style.css
-
 const scrollToAbout = () => {
   scrollToElement('.main-about');
 };
