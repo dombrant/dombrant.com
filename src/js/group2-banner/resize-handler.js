7e4981b8a6061d7a4c53ec7197ab7c219f756fbe
Dominic Brant <dombrant@gmail.com>
2018-08-09 21:36:50 -0700

    removed data-device attribute and deleted resize-handler.js

--- src/js/group2-banner/resize-handler.js
+++
@@ -1,9 +0,0 @@
-const changeDevice = () => {
-  if (window.innerWidth <= 550) {
-    document.body.dataset.device = 'mobile';
-  } else {
-    document.body.dataset.device = 'desktop';
-  }
-};
-
-window.addEventListener('resize', changeDevice);
