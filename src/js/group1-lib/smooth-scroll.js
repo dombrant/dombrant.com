629447e46f4bbe8f2bf4e9ee2afe77f16d88dc32
Dominic Brant <dombrant@gmail.com>
2019-06-21 00:46:31 -0700

    consolidated smooth-scroll.js

--- src/js/group1-lib/smooth-scroll.js
+++
@@ -1,7 +0,0 @@
-const smoothScroll = destination => {
-  window.scrollTo({
-    top: destination,
-    left: 0,
-    behavior: "smooth"
-  });
-};
--- src/js/group2-banner/1-scroll-to-element.js
+++ src/js/group2-banner/1-scroll-to-element.js
@@ -5,5 +5,9 @@ const scrollToElement = element => {
   // Get the height of the header and the top position of the element, these will be used to calculate where to scroll to when scrolling to the about section below
   // Instead of scrolling to the top of the section, it will scroll to the top of the section minus the height of the header, so that the header does not cover up the top of the section
 
-  smoothScroll(parseInt(elementTop - headerHeight));
+  window.scrollTo({
+    top: parseInt(elementTop - headerHeight),
+    left: 0,
+    behavior: "smooth"
+  });
 };
