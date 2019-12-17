f0e46cb8bbdeb447a303e8db52eb1ae60f013902
Dominic Brant <dombrant@gmail.com>
2019-12-17 12:31:56 -0800

    changed scrollToElement to no longer account for header positioning

--- src/js/group2-banner/1-scroll-to-element.js
+++ src/js/group2-banner/1-scroll-to-element.js
@@ -1,12 +1,9 @@
 const scrollToElement = element => {
-  const headerHeight = document.querySelector("header").getBoundingClientRect()
-    .height;
   const elementTop = document.querySelector(element).offsetTop;
-  // Get the height of the header and the top position of the element, these will be used to calculate where to scroll to when scrolling to the about section below
-  // Instead of scrolling to the top of the section, it will scroll to the top of the section minus the height of the header, so that the header does not cover up the top of the section
+  // Get the top position of the element, these will be used to calculate where to scroll to when scrolling to the about section below
 
   window.scrollTo({
-    top: parseInt(elementTop - headerHeight),
+    top: parseInt(elementTop),
     left: 0,
     behavior: "smooth"
   });
