199202641a04f625c9412a932d8494dc8f710a5d
Dominic Brant <dombrant@gmail.com>
2020-05-16 12:15:23 -0700

    switched to intersection observer

--- src/js/group3-main/main-sections.js
+++ src/js/group3-main/main-sections.js
@@ -1,26 +1,19 @@
-const mainDivsFadeIn = {
-  completed: false,
-
-  run() {
-    const sections = document.querySelectorAll(".main > div");
-
-    for (let [index, section] of sections.entries()) {
-      // Use destructing along with .entries to access both the element and the index below
-
-      if (isInView(section)) {
-        section
+const mainDivsFadeIn = () => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log(entry.target);
+        entry.target
           .querySelectorAll("*")
-          .forEach(element => element.classList.add("fade-in-slide-up"));
-        // From lib/fade-in-slide-up.css
-
-        if (index === sections.length - 1) {
-          // If the animation is being applied to the last element in the sections array
-
-          mainDivsFadeIn.completed = true;
-          // Set completed to true
-          // This will be used in scroll-listener.js to stop the requestAnimationFrame for this function
-        }
+          .forEach((element) => element.classList.add("fade-in-slide-up"));
+        observer.unobserve(entry.target);
       }
-    }
-  }
+    });
+  });
+
+  document
+    .querySelectorAll(".main > div")
+    .forEach((section) => observer.observe(section));
 };
+
+mainDivsFadeIn();
