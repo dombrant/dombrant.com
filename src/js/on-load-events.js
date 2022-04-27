ecd81641859053f21eae1378255e090acf14c691
Dominic Brant <dombrant@gmail.com>
2022-04-27 10:48:43 -0600

    removed banner background image load handler

--- src/js/on-load-events.js
+++ src/js/on-load-events.js
@@ -1,16 +1,5 @@
 const onLoadEvents = () => {
   window.onload = () => {
-    const backgroundImage = new Image();
-    backgroundImage.src =
-      "https://media.githubusercontent.com/media/dombrant/dombrant.com/master/img/palm-trees-6EBBDE.jpg";
-    backgroundImage.onload = () => {
-      document
-        .querySelector(".banner")
-        .classList.add("banner-image-background");
-    };
-    // Change the background of .banner from a blue color to the image with palm trees
-    // The class banner-image-background is in banner-style.css
-
     document.querySelector(".email-link").innerHTML =
       '<a href="mailto:dombrant@gmail.com?subject=Hello Dominic!"> dombrant[at]gmail[dot]com</a>';
   };
