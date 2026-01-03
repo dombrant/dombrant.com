2a875d424965edc4af405b7361796ecfcb76aa8b
Dominic Brant <dombrant@gmail.com>
2026-01-02 21:27:33 -0700

    cleaned up unnecessary files

--- src/js/on-load-events.js
+++
@@ -1,8 +0,0 @@
-const onLoadEvents = () => {
-  window.onload = () => {
-    document.querySelector(".email-link").innerHTML =
-      '<a href="mailto:dombrant@gmail.com?subject=Hello Dominic!"> dombrant[at]gmail[dot]com</a>';
-  };
-};
-
-export default onLoadEvents;
